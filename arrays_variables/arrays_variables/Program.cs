9ccaf131dcdc5b78a11af8aa718757dbf1d107bd
Michael Iwanek <miwanek11199@gmail.com>
2022-12-07 13:05:35 -0800

    added files

---
+++ arrays_variables/arrays_variables/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+public static class Program { 
+    static void Main(string[] args)
+    {
+        int i = 10, j = 20;
+
+        if (isGreater(i, j))
+        {
+            Console.WriteLine("i is less than j");
+        }
+
+        if (isGreater(j, i))
+        {
+            Console.WriteLine("j is greater than i");
+        }
+
+
+        int[] array1 = new int[5];
+
+        for (int k = 0; k < array1.Length; k++)
+        {
+            array1[k] = (k *10);
+        }
+        for (int t = 0; t < array1.Length; t++)
+        {
+            Console.WriteLine(array1[t]);
+        }
+
+    }
+
+    static bool isGreater(int i, int j)
+    {
+        return i > j;
+    }
+
+
+}
